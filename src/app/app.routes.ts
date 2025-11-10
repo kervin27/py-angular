621b974edabe5c6353b540642264cfa7aacf6a18
kervin27 <kervin.baquillas@gmail.com>
2025-11-10 21:49:58 +0100

    refactor: update staticBuild flag in railway.toml and define application routes in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // root → home
+  { path: 'home', component: App }, // route secondaria
+  { path: '**', redirectTo: 'home' }, // fallback SPA
+];
