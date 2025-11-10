aa0277b4709daa41ca52e76d1ac83cd48fc70c36
kervin27 <kervin.baquillas@gmail.com>
2025-11-10 17:22:36 +0100

    refactor: update application title and remove navbar component
    - Changed application title from 'py-angular' to 'ng-habado'.
    - Removed Navbar component and its associated files (HTML, SCSS, TypeScript, and spec).
    - Updated app.spec.ts to reflect the new title.
    - Cleaned up app.ts by removing unused imports and user data handling logic.
    - Modified index.html to change the title from 'PyAngular' to 'NgHabado' and updated the language attribute.
    - Deleted server-related files (main.server.ts and server.ts) as they are no longer needed.
    - Updated TypeScript configuration files to exclude spec files and unnecessary entries.
    - Added railway.json for deployment configuration.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,49 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Navbar } from './core/components/navbar/navbar';
-import { HttpClient } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule, FormsModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
+  styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'py-angular';
-
-  userData = signal([] as any[]);
-
-  username: string = '';
-  email: string = '';
-  password: string = '';
-
-  constructor(private http: HttpClient) {}
-
-  getUserData() {
-    this.http
-      .get('https://python-backend-production-07d8.up.railway.app/utenti')
-      .subscribe((data: any) => {
-        this.userData.set(data);
-      });
-  }
-
-  register() {
-    const user = {
-      username: this.username,
-      email: this.email,
-      password: this.password,
-    };
-
-    this.http
-      .post(
-        'https://python-backend-production-07d8.up.railway.app/utenti',
-        user
-      )
-      .subscribe((response) => {
-        this.getUserData();
-      });
-  }
+  protected title = 'ng-habado';
 }
