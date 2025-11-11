e7a663128f527b87accafaa80267c96ebd1ae100
kervin27 <kervin.baquillas@gmail.com>
2025-11-11 18:40:31 +0100

    fix: adeguamento routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,3 @@
 import { Routes } from '@angular/router';
-import { App } from './app';
 
-export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' }, // root → home
-  { path: 'home', component: App }, // route secondaria
-  { path: '**', redirectTo: 'home' }, // fallback SPA
-];
+export const routes: Routes = [];
